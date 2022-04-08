327ce5e6ef11802de5456cb756df5fca4802af41
piero <dolibiroli@gmail.com>
2022-04-08 12:13:54 -0500

    fininshed up to my_all

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,29 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return self unless block_given?
+
+    for i in 0..self.length-1 do
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    match = []
+    for i in 0..self.length-1 do
+      match.push(self[i]) if yield(self[i])
+    end
+    match
+  end
+
+  def my_all?
+    answer = true
+    for i in 0..self.length-1 do
+      answer = false unless yield(self[i], i)
+    end
+    answer
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +32,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return self unless block_given?
+
+    for i in 0..self.length-1 do
+      yield(self[i])
+    end
+    self
+  end
 end
